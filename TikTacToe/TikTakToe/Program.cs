d2ec4df5e22f1fd2fce5e6731f42f55d20db207e
m1klll <mikenegor@gmail.com>
2025-05-21 22:01:26 +0400

    Правки по ревью
    Вынес игру в отдельный класс Game
    Создал константы для обозначения полей, игроков.
    Изменил логику циклов, сделал шанс зацикливания меньше
    Сделал реструктуризацию

---
+++ TikTacToe/TikTakToe/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TikTacToe;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        const string GAME_START = "Хотите сыграть в игру? y/n: ";
+        
+        Console.Write(GAME_START);
+        {
+            while (Console.ReadLine() == "y")
+            {
+                Game game = new Game();
+                
+                string winner = game.PlayGame();
+                
+                if (winner == Game.PLAYER_X || winner == Game.PLAYER_O)
+                {
+                    Console.WriteLine($"Победил игрок {winner}!");
+                    Console.Write(GAME_START);
+                }
+                else
+                {
+                    Console.WriteLine("Ничья!");
+                    Console.Write(GAME_START);
+                }
+            }
+        }
+    }
+}
